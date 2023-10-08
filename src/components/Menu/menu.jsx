39a0f6bca24dd266a20aa2a7641c8a70ce9ab737
MN <57828955+MNgaminhhh@users.noreply.github.com>
2023-10-08 16:01:45 +0700

    update menu

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -3,17 +3,17 @@ import classes from './menu.module.css'
 import MenuPosts from '@/components/menuPosts/menuPosts'
 const menu = () => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.subtitle}>{"What's hot"}</h2>
-      <h1 className={styles.title}>Most Popular</h1>
+    <div className={classes.container}>
+      <h2 className={classes.subtitle}>{"What's hot"}</h2>
+      <h1 className={classes.title}>Most Popular</h1>
       <MenuPosts withImage={false}></MenuPosts>
       {/* /3layout */}
-      <h2 className={styles.subtitle}>Discover by topic</h2>
-      <h1 className={styles.title}>Categories</h1>
+      <h2 className={classes.subtitle}>Discover by topic</h2>
+      <h1 className={classes.title}>Categories</h1>
       <MenuCategories></MenuCategories>
       {/* 2layout */}
-      <h2 className={styles.subtitle}>{"Chosen by the editor"}</h2>
-      <h1 className={styles.title}>Editors Pick</h1>
+      <h2 className={classes.subtitle}>{"Chosen by the editor"}</h2>
+      <h1 className={classes.title}>Editors Pick</h1>
       <MenuPosts withImage={true}></MenuPosts>
     </div>
   )
