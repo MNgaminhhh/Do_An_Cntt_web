2718f8b1c6cff151a95a25030289b09d9adc21a5
MN <57828955+MNgaminhhh@users.noreply.github.com>
2023-10-17 23:47:10 +0700

    done singal page

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -43,9 +43,34 @@ const Page = () => {
 
   return (
     <div className={classes.container}>
-      {/* Display post data here */}
+      <div className={classes.infoContainer}>
+        <div className={classes.textContainer}>
+          <h1 className={classes.title}>{post.title}</h1>
+          <div className={classes.user}>
+            <div className={classes.userImageContainer}>
+              <Image className={classes.avatar} src='/p1.jpeg' alt='' fill></Image>
+            </div>
+            <div className={classes.userTextCotainer}>
+              <span className={classes.username}>MNgaminh</span>
+              <span className={classes.date}>{post.postDate}</span>
+            </div>
+          </div>
+        </div>
+        <div className={classes.imageContainer}>
+          <Image src="/p1.jpeg" alt='' fill className={classes.image}></Image>
+        </div>
+      </div>
+      <div className={classes.content}>
+        <div className={classes.post}>
+          <div className={classes.description}>
+            <p>{post.content}</p>
+          </div>
+        </div>
+          <Menu></Menu>
+      </div>
     </div>
   );
+  
 };
 
 export default Page;
