cb0e8a9f151ba467c945a39daa3a3f2a8699aa29
MN <57828955+MNgaminhhh@users.noreply.github.com>
2024-04-24 19:52:04 +0700

    update api delete userid

---
+++ src/app/api/user/route.js
@@ -0,0 +1,34 @@
+import { getAuth } from "firebase-admin/auth";
+import admin from "firebase-admin";
+const serviceAccountKey = {
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+  };
+  
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey), 
+  databaseURL: "https://manage-restaurant-d6e3c-default-rtdb.firebaseio.com"
+});
+
+export async function DELETE(req) {
+  try {
+    if (req && typeof req.json === 'function') {
+      const { uid } = await req.json();
+      await getAuth().deleteUser(uid);
+      console.log(`${uid} deleted successfully.`);
+    } else {
+      console.error("undefined");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
