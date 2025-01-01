bd4a364016e6ff71a7ad4d21fe7878c322d420c4
MN <57828955+MNgaminhhh@users.noreply.github.com>
2025-01-02 01:35:10 +0700

    update nextauth url

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["res.cloudinary.com"],
-    },
-    
-}
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
